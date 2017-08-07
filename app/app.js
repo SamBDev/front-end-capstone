c540a1d13f98b83d5f1c7801148e16105ebda453
Sam <samb283@gmail.com>
2017-08-07 10:02:55 -0500

    initial file setup

---
+++ app/app.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let abe = angular.module("Abe", ["ngRoute"])
+.constant('FirebaseUrl', 'https://pinheads-1f7c1.firebaseio.com/');
+
+let isAuth = (UserFactory)  => {
+    return new Promise( (resolve, reject) => {
+        UserFactory.isAuthenticated()
+        .then( (userExistence) => {
+            if (userExistence) {
+                resolve();
+            } else {
+                reject();
+            }
+        });
+    });
+};
+
+abe.config(($routeProvider)=>{
+    $routeProvider
+    .when('/', {
+        templateUrl: 'templates/login.html',
+        controller: 'UserController'
+    })
+    .when('/information', {
+        templateUrl: 'templates/information.html',
+        controller: 'InfoPageController',
+        resolve: {isAuth}
+    })
+    .when('/messages/:user', {
+        templateUrl : 'templates/user-messages.html',
+        controller: 'MessagesController',
+        resolve: {isAuth}
+    })
+    .when('/messages/:messageId/options', {
+        templateUrl: 'templates/message-options.html',
+        controller: 'MessageOptionsControllers',
+        resolve: {isAuth}
+    })
+    .otherwise('/');
+});
